c5d163ec44a15939e1344e6c226c68b82649ca45
Leon Yang <192827869@qq.com>
2018-05-08 10:23:26 +0800

    0.1

--- data.py
+++ data.py
@@ -1,11 +1,11 @@
 from __future__ import print_function, division
 import os
 import torch
-import pandas as pd
+# import pandas as pd
 import random
-from skimage import io, transform
+# from skimage import io, transform
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms, utils
 from PIL import Image
